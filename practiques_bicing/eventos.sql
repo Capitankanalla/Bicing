9691f44efaafdf5a1f59f1bebeb93eea1e238c07
Alex <nanoxtechnologies@gmail.com>
2026-02-05 17:01:23 +0100

    Events enllestit

--- practiques_bicing/eventos.sql
+++ practiques_bicing/eventos.sql
@@ -1,20 +1,16 @@
 -- EVENTOS
 
--- 1. event_estat_bateries  -- verificar els estats de les bateries cada cop que s´utilitzen les bicis
+-- 1. event_estat_bateries  
+-- verifica els estats de les bateries cada hora
 
--- 3. actualitzar_estat_bici -- si una bici té incidents o l´usuari informa d´un inciedn, gestionmar retirada immediata
--- 4. bloquejar_bici  -- una bici amb incidencia oberta no es pot assignar a cap usuari.
--- 5. validar_estat_bici -- una bici arreglada pot tornar a possar-se al dock
+CREATE EVENT IF NOT EXISTS event_estat_bateries
+ON SCHEDULE EVERY 1 HOUR
+DO
+    UPDATE BICICLETA
+    SET incident_acumulats = incident_acumulats + 1    
+    WHERE electrica = 1 AND batt_lvl < 20;
 
-
--- 2. reset_estacions  -- Cada nit a les 03:00 reiniciar els docks
-/* 
-event per actulitzar els dpocks lliure cada dia, per si de cas alguna cosa hagu´ñes anat mala,ment 
-    ha sigut util mentre feiem proves quan els inserts 
-    no tenien el trigger de sumar i restar al deixcar/recollir bicicletes
-*/
-
-
+-- 2. actualitzar els docks lliures
 drop event if exists repassa_llocs_lliures ;
 delimiter //
 create event repassa_llocs_lliures 
@@ -39,3 +35,29 @@ begin
     end while;
 end //
 delimiter ;
+
+
+-- -- 3. avisar usuaris inactius
+CREATE EVENT IF NOT EXISTS neteja_usuaris_inactius
+ON SCHEDULE EVERY 1 DAY
+DO
+    CALL gestionar_usuaris_inactius();
+-- 4. bloquejar_bici es repassa cada hora l´estat de la flota de bicis  
+-- una bici amb incidencies obertes no es pot assignar a cap usuari.
+CREATE EVENT IF NOT EXISTS estat_bici
+ON SCHEDULE EVERY 1 HOUR
+DO
+    UPDATE BICICLETA
+    SET ESTACIO_idESTACIO = NULL   -- bici retirada del servei
+    WHERE incident_acumulats >= 3;
+
+
+-- 5. validar_estat_bici -- una bici arreglada pot tornar a possar-se al dock
+CREATE EVENT IF NOT EXISTS event_validar_estat_bici
+ON SCHEDULE EVERY 1 HOUR
+DO
+    UPDATE BICICLETA
+    SET ESTACIO_idESTACIO = 1
+    WHERE incident_acumulats = 0 AND ESTACIO_idESTACIO IS NULL;
+
+
