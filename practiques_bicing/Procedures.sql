c6adc30b47f050d6db922795c3220a4480b884bc
Alex <nanoxtechnologies@gmail.com>
2026-02-05 19:14:38 +0100

    Actualitzacio

--- practiques_bicing/Procedures.sql
+++ practiques_bicing/Procedures.sql
@@ -1,38 +1,39 @@
 -- Generem un procediment per cada cop que un usuari agafa una bici executi el procediment per
 -- fer un trajecte.
-
 DELIMITER //
 
-CREATE PROCEDURE generar_trajecte()
+CREATE PROCEDURE generar_trajectes(IN num INT)
 BEGIN
-    INSERT INTO TRAJECTE 
-    (Hora_inici, Hora_final, dock_final_lliure, 
-     id_EST_origen, id_EST_fin,
-     BICICLETA_idBICICLETA, USUARI_idUSUARI)
-    SELECT
-        DATE_SUB(NOW(), INTERVAL FLOOR(RAND() * 720) MINUTE),
-        DATE_ADD(NOW(), INTERVAL FLOOR(RAND() * 720) MINUTE),
-        1,
-        FLOOR(1 + (RAND() * 50)),
-        FLOOR(1 + (RAND() * 50)),
-        b.idBICICLETA,
-        FLOOR(1 + (RAND() * 15))
-    FROM BICICLETA b
-    JOIN DOCK d ON d.bicicleta_idBICICLETA = b.idBICICLETA
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM TRAJECTE t
-        WHERE t.BICICLETA_idBICICLETA = b.idBICICLETA
-          AND t.Hora_final IS NULL
-    )
-    ORDER BY RAND()
-    LIMIT 1;
+    DECLARE i INT DEFAULT 0;
+
+    WHILE i < num DO
+
+        INSERT INTO TRAJECTE 
+        (Hora_inici, Hora_final, dock_final_lliure, id_EST_origen, id_EST_fin,
+         BICICLETA_idBICICLETA, USUARI_idUSUARI)
+        SELECT
+            DATE_SUB(NOW(), INTERVAL FLOOR(RAND() * 720) MINUTE),  -- hora inici aleatòria
+            NULL,                                                  -- trajecte encara no acabat
+            1,
+            FLOOR(1 + (RAND() * 50)),                              -- estació origen
+            NULL,                                                  -- estació final desconeguda
+            b.idBICICLETA,                                         -- bici aleatòria
+            FLOOR(1 + (RAND() * 15))                               -- usuari aleatori
+        FROM bicicleta b
+        ORDER BY RAND()
+        LIMIT 1;
+
+        SET i = i + 1;
+    END WHILE;
+
 END//
 
 DELIMITER ;
 
 
-CALL generar_trajecte();
+
+
+-- CALL generar_trajecte(5);
 
 -- SELECT * FROM TRAJECTE ORDER BY idTRAJECTE DESC LIMIT 5;
 
